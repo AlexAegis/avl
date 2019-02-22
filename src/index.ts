4f61b267516314742c2aa18196e4a45cf308739c
Győri Sándor <alexaegis@gmail.com>
2019-02-22 03:06:46 +0100

    index empied

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 export { Tree } from './main/tree.class';
 export { Comparable } from './main/comparable.interface';
 export { Convertable } from './main/convertable.interface';
-
-const n = 8;
-const min = Math.ceil(Math.log2(n + 1));
-const max = Math.floor(1.44 * Math.log2(n + 2) - 0.328);
-console.log(`n: ${n} min: ${min} max: ${max}`);
